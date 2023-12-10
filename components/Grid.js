81ec5b1612bf6277fe2085eeba840b7431888d47
sarahfrankel21 <sefranke@syr.edu>
2023-12-10 15:04:57 -0500

    lesson videos for thursday

---
+++ components/Grid.js
@@ -0,0 +1,8 @@
+import styles from './grid.module.scss';
+
+const Grid = ({children}) => {
+    return <section className={styles.grid}>
+        {children}
+    </section>
+}
+export default Grid;
--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,46 +1,75 @@
-import Container from'../../components/Container';
+import { useState } from 'react';
+import Container from '../../components/Container';
+import FilterBar from '../../components/FilterBar';
 import Grid from '../../components/Grid';
 import Image from 'next/image';
-import Layout from '../../components/Layout'
-import { getAllVehicles } from '../../lib/api';
-import Link from "next/link";
+import Layout from '../../components/Layout';
+import Link from 'next/link';
 
-export async function getStaticProps(){
+import { getAllVehicles, getVehicleTypes } from '../../lib/api';
+
+export async function getStaticProps() {
     const vehicles = await getAllVehicles();
-    return{
-        props:{
-            vehicles
+    const vehicleTypes = await getVehicleTypes();
+
+    vehicleTypes.unshift({
+        "node": {
+          "name": "All",
+          "slug": "all"
+        }
+      },);
+
+    return {
+        props: {
+            vehicles,
+            vehicleTypes
         }
     }
 }
-const VehiclesPage = ({ vehicles }) => {
+const VehiclesPage = ({ vehicles, vehicleTypes }) => {
+    // add "all" to vehicleTypes
+    const [activeVehicleType, setActiveVehicleType] = useState('all');
+
+    // filter vehicles by activeVehicleType
+    const filteredVehicles = activeVehicleType === 'all' ? 
+        vehicles 
+        : 
+        vehicles.filter((vehicle) => {
+            const { vehicleTypes } = vehicle.node;
+            const vehicleTypeSlugs = vehicleTypes.edges.map((vehicleType) => {
+                return vehicleType.node.slug;
+            });
+            return vehicleTypeSlugs.includes(activeVehicleType);
+     });
+
     return <Layout>
         <h1>Vehicles</h1>
         <Container>
-            <Grid> 
-
-        <ul>
-            {vehicles.map((vehicle, index) => {
-                const { title, slug, vehicleInformation} = vehicle.node;
-                const{trimLevels}=vehicleInformation;
-                return <li key={index}>
-                    {trimLevels&& trimLevels[0].images.thumbnail&&
-                    <Image
-                    scr={trimLevels[0].images.thumbnail.node.sourceUrl}
-                    alt={trimLevels[0].images.thumbnail.node.altText}
-                    width={trimLevels[0].images.thumbnail.node.mediaDetails.width}
-                    height={trimLevels[0].images.thumbnail.node.mediaDetails.height}
-                    />
-                    }
-                    <h3>{title}</h3>
-                   
-                    <p>
-                        <Link href={`/vehicles/${slug}`}>Learn more</Link>
-                    </p>
-                </li>
-            })}
-        </ul>
-        </Grid>
+            <FilterBar 
+                items={vehicleTypes} 
+                activeItem={activeVehicleType} 
+                setActiveItem={setActiveVehicleType}
+            />
+            <Grid>
+                {filteredVehicles.map((vehicle, index) => {
+                    const { title, slug, vehicleInformation } = vehicle.node;
+                    const { trimLevels } = vehicleInformation;
+                    return <article key={index}>
+                        {trimLevels && trimLevels[0].images.thumbnail && 
+                            <Image 
+                                src={trimLevels[0].images.thumbnail.node.sourceUrl}
+                                alt={trimLevels[0].images.thumbnail.node.altText}
+                                width={trimLevels[0].images.thumbnail.node.mediaDetails.width}
+                                height={trimLevels[0].images.thumbnail.node.mediaDetails.height}
+                            />
+                        }
+                        <h3>{title}</h3>
+                        <p>
+                            <Link href={`/vehicles/${slug}`}>Learn more</Link>
+                        </p>
+                    </article>
+                })}
+            </Grid>
         </Container>
     </Layout>
 }  

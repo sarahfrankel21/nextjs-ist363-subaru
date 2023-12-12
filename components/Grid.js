d652405b8f21ce206287e12551bdf35393aaaf59
sarahfrankel21 <sefranke@syr.edu>
2023-12-12 18:25:05 -0500

    motion

--- components/Grid.js
+++ components/Grid.js
@@ -1,8 +1,29 @@
 import styles from './grid.module.scss';
+import Image from 'next/Image';
+import link from 'next/link';
+import {motion} from "framer-motion";
 
-const Grid = ({children}) => {
-    return <section className={styles.grid}>
-        {children}
-    </section>
+const Grid = ({items}) => {
+    return <motion.section className={styles.grid}>
+        {items.map((item, index) => {
+                    const { title, slug, vehicleInformation } = item.node;
+                    const { trimLevels } = vehicleInformation;
+                    return <motion.article key={index}>
+                        {trimLevels && trimLevels[0].images.thumbnail && 
+                            <Image 
+                                src={trimLevels[0].images.thumbnail.node.sourceUrl}
+                                alt={trimLevels[0].images.thumbnail.node.altText}
+                                width={trimLevels[0].images.thumbnail.node.mediaDetails.width}
+                                height={trimLevels[0].images.thumbnail.node.mediaDetails.height}
+                            />
+                        }
+                        <h3>{title}</h3>
+                        <p>
+                            <Link href={`/vehicles/${slug}`}>Learn more</Link>
+                        </p>
+                    </motion.article>
+                })}
+        
+    </motion.section>
 }
 export default Grid;
--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import Container from '../../components/Container';
 import FilterBar from '../../components/FilterBar';
 import Grid from '../../components/Grid';
@@ -51,26 +50,10 @@ const VehiclesPage = ({ vehicles, vehicleTypes }) => {
                 activeItem={activeVehicleType} 
                 setActiveItem={setActiveVehicleType}
             />
-            <Grid>
-                {filteredVehicles.map((vehicle, index) => {
-                    const { title, slug, vehicleInformation } = vehicle.node;
-                    const { trimLevels } = vehicleInformation;
-                    return <article key={index}>
-                        {trimLevels && trimLevels[0].images.thumbnail && 
-                            <Image 
-                                src={trimLevels[0].images.thumbnail.node.sourceUrl}
-                                alt={trimLevels[0].images.thumbnail.node.altText}
-                                width={trimLevels[0].images.thumbnail.node.mediaDetails.width}
-                                height={trimLevels[0].images.thumbnail.node.mediaDetails.height}
-                            />
-                        }
-                        <h3>{title}</h3>
-                        <p>
-                            <Link href={`/vehicles/${slug}`}>Learn more</Link>
-                        </p>
-                    </article>
-                })}
-            </Grid>
+            <Grid
+                items={filteredVehicles}
+                
+            />
         </Container>
     </Layout>
 }  
